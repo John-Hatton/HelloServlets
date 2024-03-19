63778d76ff48b4a64fe93c1b32d6654959f2488a
John Hatton <John.Hatton@web.de>
2024-03-19 19:33:54 +0100

    Remove XML and fix annotation

--- src/main/java/mars/hatton/servlet/HelloServlet.java
+++ src/main/java/mars/hatton/servlet/HelloServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
+@WebServlet(name = "HelloServlet", urlPatterns = {"/HelloServlet"})
 public class HelloServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
